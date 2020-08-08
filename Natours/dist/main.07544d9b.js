8ecbed66adda4c0b4efc367e3650d3598945545a
timcodes <hardtimo@isu.edu>
2020-08-08 16:59:41 -0600

    implmented custom float grid system

--- Natours/dist/main.07544d9b.js
+++ Natours/dist/main.07544d9b.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64587" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59151" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
